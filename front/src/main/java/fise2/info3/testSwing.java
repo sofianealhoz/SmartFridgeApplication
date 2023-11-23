b915dfcda8956a1521dd07d7d2e196db90858277
ridhoi.nolwen <nolwen.ridhoi.ts1@gmail.com>
2023-11-23 11:06:33 +0100

    set full screen

--- front/src/main/java/fise2/info3/testSwing.java
+++ front/src/main/java/fise2/info3/testSwing.java
@@ -8,7 +8,8 @@ public class testSwing {
 	public static void main(String[] args) {
 		JFrame window = new JFrame("Première appli");
 	    window.add(new JLabel("Hello world!"));
-	    window.setSize(700,500);
+	    //window.setSize(700,500);
+	    window.setExtendedState(JFrame.MAXIMIZED_BOTH);
 	    window.setLocationRelativeTo(null);
 	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    window.setVisible(true);
