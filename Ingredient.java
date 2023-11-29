c1893f2c0ee3f8a25c1b300a4cebd875f89e950d
DESPREZ <thomas.desprez50840@outlook.fr>
2023-11-29 15:13:40 +0000

    Added Ingredient.java

---
+++ Ingredient.java
@@ -0,0 +1,36 @@
+package projet;
+
+import java.time.LocalDate;
+
+class Ingredient {
+    private String nom;
+    private LocalDate datePeremption;
+    private int quantite;
+
+    public Ingredient(String nom, LocalDate datePeremption, int quantite) {
+        this.nom = nom;
+        this.datePeremption = datePeremption;
+        this.quantite = quantite;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public LocalDate getDatePeremption() {
+        return datePeremption;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    // Ajoutez la méthode equals pour pouvoir comparer deux ingrédients
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Ingredient ingredient = (Ingredient) obj;
+        return nom.equals(ingredient.nom);
+    }
+}
